044350b00f286fb3c4c81703585b024c9d74f107
tanssive <tanssive@yahoo.com>
2020-07-21 19:42:35 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,9 @@
 import string
 import random
 
-def generate_ascii_characters_list():
-    characters = list(string.printable)
-    chr_lst = characters[:len(characters)-6]
-    return chr_lst
+def generate_random_password(amnt_of_char):
+    print(''.join(random.choice(string.printable) for _ in range(amnt_of_char)))
 
-def create_random_passwd(amnt_of_char):
-    lst_of_chars = generate_ascii_characters_list()
+generate_random_password(int(input('Provide length of the new password:')))
 
-    return ''.join(random.choice(lst_of_chars) for _ in range(amnt_of_char))
 
-specified_len = int(input('Provide length of the new password: '))
-
-print(create_random_passwd(specified_len))
